7d03b0835fee2dd5dc8730390936f8efbe847f4d
Matthijs Wagemakers <wagemakers.m@gmail.com>
2020-10-04 18:49:22 +0200

    quick app

---
+++ SampleWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SampleWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string MachineName => Environment.MachineName;
+        public string OS => RuntimeInformation.OSDescription;
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
